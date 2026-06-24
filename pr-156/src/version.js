7eed4f6ca72b8ee1728140ba08c5988772beb3c0
devvi <devvi@users.noreply.github.com>
2026-06-24 14:41:52 +0000

    deploy: 20bbe287c757c51005e0468b8876fcff0be5457a

--- pr-156/src/version.js
+++
@@ -1,2 +0,0 @@
-export const GIT_HASH = '4ff0d1c PR #156';
-export const GIT_DATE = '2026-06-24';
--- src/version.js
+++ src/version.js
@@ -1,2 +1,2 @@
-export const GIT_HASH = '2c8b33c';
+export const GIT_HASH = '20bbe28';
 export const GIT_DATE = '2026-06-24';
